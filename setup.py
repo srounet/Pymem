438342d93386dfd00731796f6f1e4cf2bf1575f4
Fabien Reboia <srounet@gmail.com>
2015-06-12 12:20:31 +0200

    nump to 0.3 due to pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='Pymem',
-    version='0.2c',
+    version='0.3',
     description='pymem: python memory access made easy',
     author='Fabien Reboia',
     author_email='srounet@gmail.com',

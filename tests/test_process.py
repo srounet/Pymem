92b2b04419c8e4c9d5d4436b4ba3ded105feafcb
Fabien Reboia <srounet@gmail.com>
2018-05-23 19:53:26 +0200

    add test files

---
+++ tests/test_process.py
@@ -0,0 +1,24 @@
+import pymem
+import pymem.exception
+import pymem.process
+
+import pytest
+
+
+def test_existing_process():
+    pm = pymem.Pymem('explorer.exe')
+    assert pm.process_handle
+
+
+def test_missing_process():
+    with pytest.raises(pymem.exception.ProcessNotFound):
+        pymem.Pymem('missing.exe')
+
+
+def test_process_modules():
+    pm = pymem.Pymem('explorer.exe')
+    modules = pymem.process.enum_process_module(
+        pm.process_handle
+    )
+    modules = list(modules)
+    assert len(modules)

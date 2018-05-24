3d724597b9ccd531ceda523661151f4b4e1604c5
Fabien Reboia <srounet@gmail.com>
2018-05-24 08:14:08 +0200

    enforce test on process modules

--- tests/test_process.py
+++ tests/test_process.py
@@ -15,10 +15,18 @@ def test_missing_process():
         pymem.Pymem('missing.exe')
 
 
-def test_process_modules():
+def test_list_process_modules():
     pm = pymem.Pymem('python.exe')
     modules = pymem.process.enum_process_module(
         pm.process_handle
     )
     modules = list(modules)
     assert len(modules)
+    assert 'python3.dll' in [
+        m.name for m in modules
+    ]
+
+
+def test_process_base():
+    pm = pymem.Pymem('python.exe')
+    assert pm.process_base.name == 'python.exe'

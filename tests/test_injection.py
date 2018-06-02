f382c0f90a6f3647456be0be352efd2d65b84f02
Fabien Reboia <srounet@gmail.com>
2018-06-02 12:25:48 +0200

    try to test injection with appveyor

--- tests/test_injection.py
+++ tests/test_injection.py
@@ -1,8 +1,24 @@
+import os.path
 import pymem
 
 
-def test_existing_process():
+def test_inject_python_interpreter():
     pm = pymem.Pymem('python.exe')
     pm.inject_python_interpreter()
 
     assert pm.py_run_simple_string
+
+
+def test_inject_python_shellcode():
+    pm = pymem.Pymem('python.exe')
+    pm.inject_python_interpreter()
+
+    assert pm.py_run_simple_string
+    pm.inject_python_shellcode("""
+f = open("C:\\\\pymem_injection.txt", "w+")
+f.write("pymem_injection")
+f.close()
+    """)
+    assert os.path.exists("C:\pymem_injection.txt")
+
+

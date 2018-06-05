ebaaa07ec158d930103efac0a2d3b4e53cd5c5a1
Fabien Reboia <srounet@gmail.com>
2018-06-05 20:57:26 +0200

    add test file for read/writes

---
+++ tests/test_memory.py
@@ -0,0 +1,130 @@
+import decimal
+import pymem
+
+
+def test_allocate():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    assert address
+
+
+def test_write_int():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_int(address, 1337)
+    assert pm.read_int(address) == 1337
+
+
+def test_write_uint():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_uint(address, 1337)
+    assert pm.read_uint(address) == 1337
+
+
+def test_write_short():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_short(address, 1)
+    assert pm.read_short(address) == 1
+
+
+def test_write_ushort():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_ushort(address, 1)
+    assert pm.read_ushort(address) == 1
+
+
+def test_write_float():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_float(address, 13.3)
+    result = decimal.Decimal(
+        pm.read_float(address)
+    )
+    result = decimal.Decimal(
+        result.quantize(
+            decimal.Decimal('.01'),
+            rounding=decimal.ROUND_HALF_UP
+        )
+    )
+    expected = decimal.Decimal('13.30')
+    expected = decimal.Decimal(
+        expected.quantize(
+            decimal.Decimal('.01'),
+            rounding=decimal.ROUND_HALF_UP
+        )
+    )
+    assert result == expected
+
+
+def test_write_long():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_long(address, 1337)
+    assert pm.read_long(address) == 1337
+
+
+def test_write_ulong():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_ulong(address, 1337)
+    assert pm.read_ulong(address) == 1337
+
+
+def test_write_longlong():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_longlong(address, 1337)
+    assert pm.read_longlong(address) == 1337
+
+
+def test_write_ulonglong():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_ulonglong(address, 1337)
+    assert pm.read_ulonglong(address) == 1337
+
+
+def test_write_double():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_double(address, 13.37)
+    assert pm.read_double(address) == 13.37
+
+
+def test_write_string():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_string(address, "pymem")
+    assert pm.read_string(address) == "pymem"
+
+
+def test_write_char():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_char(address, "s")
+    assert pm.read_char(address) == "s"
+
+
+def test_write_uchar():
+    pm = pymem.Pymem('python.exe')
+    address = pm.allocate(10)
+
+    pm.write_uchar(address, 114)
+    assert pm.read_uchar(address) == 114
+

f7f3b1ef16f33bdcf83c92f83bc1aabdc2ee06f4
StarrFox <starrfox6312@gmail.com>
2020-08-03 15:17:54 -0500

    Fix print statements in process.py and add write_bytes to Pymem class (#26)
    * Fix requirements issues
    
    * Add write_bytes to Pymem and fix prints to be logs

--- pymem/__init__.py
+++ pymem/__init__.py
@@ -782,6 +782,36 @@ class Pymem(object):
             raise pymem.exception.MemoryReadError(address, byte, e.error_code)
         return value
 
+    def write_bytes(self, address, value, length):
+        """Write `value` to the given `address` into the current opened process.
+
+        Parameters
+        ----------
+        address: int
+            An address of the region of memory to be written.
+        value: bytes
+            the value to be written
+        length: int
+            Number of bytes to be written
+
+        Raises
+        ------
+        ProcessError
+            If there id no opened process
+        MemoryWriteError
+            If WriteProcessMemory failed
+        TypeError
+            If address is not a valid integer
+        """
+        if not self.process_handle:
+            raise pymem.exception.ProcessError('You must open a process before calling this method')
+        if value is None or not isinstance(value, bytes):
+            raise TypeError('Invalid argument: {}'.format(value))
+        try:
+            pymem.memory.write_bytes(self.process_handle, address, value, length)
+        except pymem.exception.WinAPIError as e:
+            raise pymem.exception.MemoryWriteError(address, value, e.error_code)
+
     def write_int(self, address, value):
         """Write `value` to the given `address` into the current opened process.
 

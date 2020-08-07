85af90bc4b6fa95725e1770c7c846a0ea62fb040
srounet <srounet@gmail.com>
2020-08-07 19:08:15 +0200

    link submenus in leftnavbar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,7 +106,7 @@ html_theme_options = {
     # Toc options
     'collapse_navigation': False,
     'sticky_navigation': True,
-    'navigation_depth': 1,
+    'navigation_depth': 2,
     'includehidden': True,
     'titles_only': False,
     'nosidebar': True,

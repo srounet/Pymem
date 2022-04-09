47d7bffaf7de44397d7914ee4a1f81138ab23fa3
srounet <srounet@gmail.com>
2022-04-09 17:47:46 +0200

    user_space_limit

--- pymem/pattern.py
+++ pymem/pattern.py
@@ -156,7 +156,8 @@ def pattern_scan_all(handle, pattern, *, return_multiple=False):
     next_region = 0
 
     found = []
-    while next_region < 0x7FFFFFFF:
+    user_space_limit = 0x7FFFFFFF0000 if sys.maxsize > 2**32 else 0x7fff0000
+    while next_region < user_space_limit:
         next_region, page_found = scan_pattern_page(
             handle,
             next_region,

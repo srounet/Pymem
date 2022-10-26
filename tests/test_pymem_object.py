782a5a80f71acd04d8449276b7d1e364044cb267
StarrFox <starrfox6312@gmail.com>
2022-10-26 12:29:43 -0500

    allow initing Pymem objects with process id

---
+++ tests/test_pymem_object.py
@@ -0,0 +1,21 @@
+import os
+
+import pytest
+
+import pymem
+
+
+def test_init_process_by_name():
+    process = pymem.Pymem("python.exe")
+    assert process.base_address is not None
+
+
+def test_init_process_by_id():
+    process = pymem.Pymem(os.getpid())
+    assert process.base_address is not None
+    assert process.process_id == os.getpid()
+
+
+def test_error_init_non_str_int():
+    with pytest.raises(TypeError):
+        pymem.Pymem(1.0)
